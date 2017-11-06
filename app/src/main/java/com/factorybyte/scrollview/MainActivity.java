ec94e4c1ad5d5605427f1e969bb150bf5f2eb04b
Jorge Montellanos Díaz <jorgemontellanos@gmail.com>
2017-11-06 12:33:47 -0400

    con buscador

--- app/src/main/java/com/factorybyte/scrollview/MainActivity.java
+++ app/src/main/java/com/factorybyte/scrollview/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.SearchView;
 
 import com.factorybyte.scrollview.Adapters.MyAdapter;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     GridView grid;
+    SearchView search;
 
     int images[] = {R.drawable.uno, R.drawable.dos, R.drawable.tres, R.drawable.cuatro, R.drawable.cinco};
     String textos []= {"Mi texto cargado", "Texto 2", "tres", "cuatro", "cinco"};
@@ -26,7 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        grid = (GridView) findViewById(R.id.grid);
+        search = (SearchView) findViewById(R.id.search);
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("data", "onQueryTextSubmit: " + query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("data", "onQueryTextChange: " + newText);
+                return false;
+            }
+        });
+
+
+
+        /*grid = (GridView) findViewById(R.id.grid);
 
 
         MyAdapter adapter = new MyAdapter(this.getApplicationContext(), images, textos);
@@ -49,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+*/
 
 
 
